abf75a413f209fe7a6b2b7372b300836a82f435a
Katykaze <kathezambrano9@gmail.com>
2022-09-23 13:48:26 +0200

    ejercicio13

--- practica-5-13/practica-05-13.js
+++ practica-5-13/practica-05-13.js
@@ -5,6 +5,7 @@ function main() {
 function validar() {
     let palindromo = document.formulario.palindromo.value;
     document.formulario.mensaje.value = validarFrase(palindromo) ? "Palíndromo" : "No es Palíndromo";
+    document.formulario.mensaje.value = cadenaInversa(palindromo);
 }
 function validarFrase(palindromo) {
     return validarPalindromo(palindromo);
@@ -15,9 +16,10 @@ function validarPalindromo(palindromo) {
     
     //quitar los espacios para ello primero localizamos los espacios con split y luego usamos join para la nueva
     //cadena sin espacios
-    let palindromoSinEspacios = palindromo.split(" ").join("");
+    let palindromoSinEspacios = (palindromo.split(" ").join("")).toLowerCase();
     console.log(palindromoSinEspacios);
     let copiaPalindromo = palindromoSinEspacios;
+    //llamamos a la función que le dará la vuelta a la cadena
     console.log(cadenaInversa(copiaPalindromo));
     if(palindromoSinEspacios == cadenaInversa(copiaPalindromo)){
         valido = true;
