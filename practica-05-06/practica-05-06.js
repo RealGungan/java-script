052a3c34e807787d1e7e9fb3fba91f22f60fa59a
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-09-24 12:48:09 +0200

    dani_fin

--- practica-05-06/practica-05-06.js
+++ practica-05-06/practica-05-06.js
@@ -5,18 +5,26 @@ function inicio() {
 }
 
 function comprobar() {
-    debugger;
 
+    //variable para detectar la cadena en la que vamos a buscar similitudes
     var cadena = document.forms.form.cadena.value;
+    //variable si la subcadena es de un único carácter
     var cadena_corta = document.forms.form.subcadena.value;
+    //variable si la subcadena es de más de un carácter
     var cadena_larga = document.forms.form.subcadena.value;
+    //variable para contar cuántas veces aparece la subcadena
     var count = 0;
 
+    //si la subcadena es de más dee un carácter trbajamos con la variable "cadena_larga".
+    //Por el contratio, utilizaremos la variable "cadena_corta"
     if (cadena_corta.length > 1) {
+        //comntamos cuantas veces aparece la subcadena en la cadena
         count = cadena.split(cadena_larga).length - 1;
     } else {
+        //comntamos cuantas veces aparece la subcadena en la cadena
         count = cadena.split(cadena_corta).length - 1;
     }
 
+    //mostramos el resultado en la caja "Mensaje"
     document.forms.form.mensaje.value = count;
 }
