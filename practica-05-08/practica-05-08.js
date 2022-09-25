3e4d2068e6bae009744c4d14ad75006749c7f06d
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-09-25 17:38:02 +0200

    fix 8

--- practica-05-08/practica-05-08.js
+++ practica-05-08/practica-05-08.js
@@ -17,9 +17,9 @@ function comprobar() {
     //locate the day
     var index = date.indexOf("/", 0);
     var day = date.substring(0, index);
-    console.log(day);
     //validate the day
     if (isNaN(day) || day.length > 2 || day < 0 || day > 31) {
+        document.forms.form.mensaje.value = false;
         error = 1;
     } else {
         //locate the month
@@ -27,12 +27,14 @@ function comprobar() {
         var mes = date.substring(index + 1, index_mes);
         //validate the month
         if (isNaN(mes) || mes.length > 2 || mes < 0 || mes > 12) {
+            document.forms.form.mensaje.value = false;
             error = 1;
         } else {
             //locate the year
             var ano = date.substring(index_mes + 1, date.length);
             //validate the year
             if (isNaN(date.substring(index_mes + 1, date.length)) || ano.length > 4) {
+                document.forms.form.mensaje.value = false;
                 error = 1;
             }
         }
