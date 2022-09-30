77d82efe0f7be2a42019bacf3348dde736f4bb11
RealGungan <69304877+RealGungan@users.noreply.github.com>
2022-09-30 14:24:20 +0200

    folders

---
+++ PrimerTrimestre/javascript-01/practica-05-07/practica-05-07.js
@@ -0,0 +1,31 @@
+window.onload = inicio;
+
+function inicio() {
+    document.forms.form.button.onclick = comprobar;
+}
+
+
+function comprobar() {
+    var nombre = document.forms.form.nombre.value;
+
+    //crear un buble para comprobar si la longitud está comprendida entre 3 y 27
+    //y, para averiguar si el primer y último carácter son números.
+    //Si alguna de estas condiciones falla, el programa termina y da error.
+    if (nombre.length >= 3 && nombre.length <= 27
+        && !isNaN(nombre.substring(0, 1)) &&
+        !isNaN(nombre.substring(nombre.length - 1, nombre.length))) {
+        //recorremos la variable "nombre" para ver si contiene algún número.
+        //si es así, da error
+        for (var i = 0; i < nombre.length; i++) {
+            if (!isNaN(nombre[i])) {
+                document.forms.form.mensaje.value = false;
+                continue;
+            } else {
+                document.forms.form.mensaje.value = true;
+            }
+        }
+    } else {
+        document.forms.form.mensaje.value = false;
+    }
+
+}
