9485d5ddb6ac682d43ca5eed71b7d6f8db36b37d
Katykaze <kathezambrano9@gmail.com>
2022-10-07 14:16:54 +0200

    cod empresa y direccion

--- PrimerTrimestre/RETO_2/js/ocho.js
+++ PrimerTrimestre/RETO_2/js/ocho.js
@@ -11,7 +11,7 @@ function validarFormulario() {
         alert(mensaje);
         enviar = false;
     }
-    
+
     return enviar;
 
 }
@@ -27,6 +27,19 @@ function validarDni_Cif() {
     return validacionNif;
 }
 
+function validarCodEmp() {
+    let codigo_empresa = document.formulario.codigo_empresa.value;
+    let codigo_emp = codigo_empresa.toLowerCase().trim();
+    let valido = true;
+    if (codigo_emp.length < 5 || codigo_emp.length > 10) {
+        //document.getElementById("codigo_empresa").style.borderColor = "red";
+        valido = false;
+    }
+    if (codigo_emp.charAt(0) < "a" || codigo_emp.charAt(0) > "z") {
+        valido = false;
+    }
+    return valido;
+}
 
 function validarRazon() {
     debugger;
@@ -60,4 +73,22 @@ function validarRazon() {
     }
 
     return cadena_errores;
+}
+
+function validarDirec() {
+    let direccion = document.formulario.direccion.value;
+    let direcc = direccion.toLowerCase().trim();
+    let valido = true;
+    let caracteres = "ºª-/.";
+    if (direcc.charAt(0) < "a" || direcc.charAt(0) > "z") {
+        valido = false;
+    }
+    let ultimo = direcc.charAt(direcc.length - 1);
+    console.log(ultimo);
+    if (caracteres.includes(ultimo)) {
+        valido = false;
+    }
+
+    return valido;
+
 }
